071d0c924a108740c3ed643c54e57f8a0da2b5c2
dnagor <dnagor0@gmail.com>
2021-11-01 11:54:23 +0200

    Initial commit

---
+++ src/main/java/homework/Days.java
@@ -0,0 +1,13 @@
+package homework;
+
+public enum Days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+
+}
---
+++ src/main/java/homework/IncorrectTimeException.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class IncorrectTimeException extends Exception {
+    public IncorrectTimeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
